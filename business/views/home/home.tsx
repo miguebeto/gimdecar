3faa0840bdbc5cb63356262bdcef16a1e5973dab
Miguel Blanco <78479657+miguebeto@users.noreply.github.com>
2023-04-13 08:30:05 -0500

    add banner with background

--- business/views/home/home.tsx
+++ business/views/home/home.tsx
@@ -15,8 +15,6 @@ const HomePage = () => {
       <h1>Hola mundo</h1>
       <h1>Hola mundo</h1>
       <div className={styles.background_content}>
-        <h1>Hola mundo</h1>
-        {/* <Image src="/images/banner.png" alt="banner" width={600} height={200} /> */}
       </div>
       <div className={styles.banner}>
         <Image className={styles.banner_img} src="/images/banner.png" alt="banner" title="banner" width={700} height={250} />
